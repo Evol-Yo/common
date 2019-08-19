07b4ae4e2deeb899b2999c5ba8994361e67b0dff
fanrenlangzi <15191432680@163.com>
2019-08-19 16:08:18 +0800

    Create run_days.sh

---
+++ shell/run_days.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+if [ $# -lt 2 ];then
+    echo "./*** <begin_date> <end_date>"
+    exit 1
+fi
+
+begin_date=`date -d "$1" +'%Y%m%d'`
+end_date=`date -d "$2" +'%Y%m%d'`
+
+retry=0
+if [ -n "$3" ];then
+    retry=$3
+fi
+
+while [ $begin_date -le $end_date ];do
+    echo $begin_date
+
+    aRetry=$retry
+    while [ $aRetry -ge 0 ];do
+
+        ## --------------------- call your command here ----------------------------
+
+
+        ## -------------------------------------------------------------------------
+
+        if [ $? -eq 0 ];then
+            break
+        fi
+
+        echo "try again! date: $begin_date"
+        aRetry=$((aRetry - 1))
+        
+        wait
+        sleep 10
+    done
+
+    if [ $aRetry -lt 0 ]; then
+        echo "超过最大重试次数，date: $begin_date"
+        exit 1
+    fi
+
+    begin_date=`date -d "$begin_date 1 days" +'%Y%m%d'`
+
+    wait
+    sleep 3
+done
